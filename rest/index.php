d5bd6185ad3199197a96a528e7f53905474f8f9a
Zara <zarabahtanovic@gmail.com>
2023-05-07 13:04:18 +0200

    rest folder

--- rest/index.php
+++ rest/index.php
@@ -1,11 +1,9 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
-require '../vendor/autoload.php';
+<?php 
+require_once '../vendor/autoload.php';
+require_once './routes/ActorsRoutes.php';
 
-require "services/ActorsService.php";
-Flight::register('actors_service', "ActorsService");
 
-require_once 'routes/ActorsRoutes.php';
 
 Flight::start();
+
+?>
