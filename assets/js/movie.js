a24889dc2e96ae7a0e92168b945463512f255b7b
Zara <zarabahtanovic@gmail.com>
2023-05-10 23:57:49 +0200

    added movie.js and movies section

---
+++ assets/js/movie.js
@@ -0,0 +1,8 @@
+// $(function(){
+//     var id=localStorage.getItem("id");
+//     console.log(id)
+//     $.get("rest/movies/id/"+id, function (data) {
+//         console.log(data);
+//     });
+
+// })
