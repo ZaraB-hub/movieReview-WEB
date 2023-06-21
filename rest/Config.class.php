d88450a72cfa82322df6a65e6d814fe3a417cffb
Zara <zarabahtanovic@gmail.com>
2023-06-21 19:41:40 +0200

    .

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,23 +1,28 @@
-<?php
+   <?php
+   
+   class Config{
 
-class Config {
-    public static function DB_HOST(){
-        return 'localhost';
-    }
-
-    public static function DB_USERNAME(){
-        return 'root';
-    }
-
-    public static function DB_PASSWORD(){
-        return 'uniburch';
-    }
-
-    public static function DB_SCHEMA(){
-        return 'zmdb';
-    }
-
-    public static function JWT_SECRET(){
-        return "some_secret";
-    }
-}
+        public static function DB_HOST(){
+          return Config::get_env("DB_HOST", "localhost");
+        }
+        public static function DB_USERNAME(){
+          return Config::get_env("DB_USERNAME", "root");
+        }
+        public static function DB_PASSWORD(){
+          return Config::get_env("DB_PASSWORD", "uniburch");
+        }
+        public static function DB_SCHEMA(){
+          return Config::get_env("DB_SCHEMA", "zmdb");
+        }
+        public static function DB_PORT(){
+          return Config::get_env("DB_PORT", "3306");
+        }
+        public static function JWT_SECRET(){
+          return "some_secret";
+        }
+      
+        public static function get_env($name, $default){
+          return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+        }
+      }
+      
