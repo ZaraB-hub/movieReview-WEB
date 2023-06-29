fe41348bb9031fae685586e8a9642a004993f095
Zara <zarabahtanovic@gmail.com>
2023-06-29 08:18:27 +0200

    format

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,28 +1,35 @@
-   <?php
-   
-   class Config{
+<?php
 
-        public static function DB_HOST(){
-          return Config::get_env("DB_HOST", "localhost");
-        }
-        public static function DB_USERNAME(){
-          return Config::get_env("DB_USERNAME", "root");
-        }
-        public static function DB_PASSWORD(){
-          return Config::get_env("DB_PASSWORD", "uniburch");
-        }
-        public static function DB_SCHEMA(){
-          return Config::get_env("DB_SCHEMA", "zmdb");
-        }
-        public static function DB_PORT(){
-          return Config::get_env("DB_PORT", "3306");
-        }
-        public static function JWT_SECRET(){
-          return "some_secret";
-        }
-      
-        public static function get_env($name, $default){
-          return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
-        }
+    class Config
+    {
+
+      public static function DB_HOST()
+      {
+        return Config::get_env("DB_HOST", "localhost");
       }
-      
+      public static function DB_USERNAME()
+      {
+        return Config::get_env("DB_USERNAME", "root");
+      }
+      public static function DB_PASSWORD()
+      {
+        return Config::get_env("DB_PASSWORD", "uniburch");
+      }
+      public static function DB_SCHEMA()
+      {
+        return Config::get_env("DB_SCHEMA", "zmdb");
+      }
+      public static function DB_PORT()
+      {
+        return Config::get_env("DB_PORT", "3306");
+      }
+      public static function JWT_SECRET()
+      {
+        return "some_secret";
+      }
+
+      public static function get_env($name, $default)
+      {
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+      }
+    }
