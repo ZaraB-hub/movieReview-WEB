bf9ce55f0af5b34e758f450ba9940bce6c011ed7
Zara <zarabahtanovic@gmail.com>
2023-07-02 20:37:14 +0200

    reviews

---
+++ assets/js/controller/review-service.js
@@ -0,0 +1,111 @@
+var ReviewService = {
+    init: function () {
+        ReviewService.getReviews();
+    },
+    getReviews:function(){
+        var mID = localStorage.getItem("selectedMovieId");
+        // var loggedInUserId = Utils.parseJwt(localStorage.getItem("user_token")).UsersID;
+        $("#reviews-container").empty();
+        $.get("rest/reviews/movie/"+mID, function (data) {
+            if (data.length === 0) {
+                $("#reviews-container").append("<h4 class='pt-4 m-auto pb-5'> No reviews made yet. Be the first one to review this movie</h4>");
+            } else {
+                data.forEach(function (review) {
+                    // console.log(review);
+                    $.get("rest/users/" + review.UsersID, function (user) {
+                        var deleteButtonHtml = (review.UsersID === Utils.parseJwt(localStorage.getItem("user_token")).UsersID) ? `<i class="bi bi-x-square " onclick=ReviewService.delete(${review.ReviewsID}) data-review-id="${review.ReviewsID}" style="cursor:pointer" ></i>` : "";
+                        var reviewHtml = `
+                        <div class="review d-flex p-2 border-bottom ">
+                            <img src="./assets/pics/images.jfif" alt="" width="50px" height="50px">
+                            <div class="d-flex" style="flex-grow: 1;">
+                                <div style="flex-grow: 1;">
+                                    <div class="person-info m-1 ms-2">
+                                        <h6 class="opacity-100">${user.Username}</h6>
+                                        <h6 class="opacity-75">${review.CreatedAt}</h6>
+                                    </div>
+                                    <p class="ms-2">${review.Comment}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    
+                        $("#reviews-container").append(reviewHtml);
+                    });
+                });
+            }
+        });
+    },
+    create: function (entity) {
+        var token = localStorage.getItem("user_token");
+        var user = Utils.parseJwt(token);
+        var payload = {
+            MoviesID: entity.MoviesID,
+            UsersID: user.UsersID,
+            Comment: entity.Comment
+        };
+        $.ajax({
+            url: "rest/reviews",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(
+                    "Authorization",
+                    localStorage.getItem("user_token")
+                );
+            },
+            type: "POST",
+            data: JSON.stringify(payload),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (result) {
+                console.log(result);
+                ReviewService.getReviews();
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log(errorThrown);
+            },
+        });
+    },
+
+    update: function (review) {
+        $.ajax({
+            url: "rest/review/" + review.id,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(
+                    "Authorization",
+                    localStorage.getItem("user_token")
+                );
+            },
+            type: "PUT",
+            data: JSON.stringify(review),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (result) {
+                console.log(result);
+                ReviewService.getReviews();
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                toastr.error(XMLHttpRequest.responseJSON.message);
+            },
+        });
+    },
+
+    delete: function (reviewId) {
+        $.ajax({
+            url: "rest/reviews/" + reviewId,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(
+                    "Authorization",
+                    localStorage.getItem("user_token")
+                );
+            },
+            type: "DELETE",
+            success: function (result) {
+                ReviewService.getReviews();                
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                toastr.error(XMLHttpRequest.responseJSON.message);
+            },
+        });
+    },
+
+
+}
